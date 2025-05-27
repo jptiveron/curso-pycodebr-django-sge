97617d62905c0980bae76d8116c7b228c961bd92
jptiveron <jptiveron@hotmail.com>
2025-05-27 13:16:44 -0300

    criadas views e urls de inflows

--- inflows/views.py
+++ inflows/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, CreateView
 
-# Create your views here.
+from inflows.forms import InflowForm
+from inflows.models import Inflow
+
+
+class InflowListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    model = Inflow
+    template_name = 'inflow_list.html'
+    context_object_name = 'inflows'
+    paginate_by = 10
+    permission_required = 'inflows.view_inflow'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        product = self.request.GET.get('product')
+
+        if product:
+            queryset = queryset.filter(product__title__icontains=product)
+
+        return queryset
+
+
+class InflowDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    model = Inflow
+    template_name = 'inflow_detail.html'
+    permission_required = 'inflows.view_inflow'
+
+
+class InflowCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    model = Inflow
+    template_name = 'inflow_create.html'
+    form_class = InflowForm
+    permission_required = 'inflows.add_inflow'
+    success_url = reverse_lazy('inflow_list')
